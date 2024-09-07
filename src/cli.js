5d13c5d97052732ca50996cb7cc3bf123c977248
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2024-09-07 14:22:24 -0300

    exportando o resultado deu bug. vamos tentando.

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,36 @@
 //A linha `import` está substituindo a linha `const fs...`
-import fs from 'fs';
+import fs from "fs";
 // const fs = require('fs'); <Está comentada pq não se faz mais necessária
 
 //A linha `import` está substituindo a linha `const trataErros...`
-import trataErros from './erros/funcoesErro.js';
-// const trataErros = require('./erros/funcoesErro'); Está comentada pq não se faz mais necessária 
+import trataErros from "./erros/funcoesErro.js";
+// const trataErros = require('./erros/funcoesErro'); Está comentada pq não se faz mais necessária
 
 //Importando a função contaPalavras
-import { contaPalavras } from './index.js';
+import { contaPalavras } from "./index.js";
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3];
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    try {
-      if (erro) throw erro;
-      contaPalavras(texto);
-    } catch(erro) {
-      trataErros(erro);
-    }
-  })
+fs.readFile(link, "utf-8", (erro, texto) => {
+  try {
+    if (erro) throw erro;
+    console.log("chegou aqui");
+    const resultado = contaPalavras(texto);
+    criaESalvaArquivo(resultado, endereco);
+  } catch (erro) {
+    trataErros(erro);
+  }
+});
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = JSON.stringify(listaPalavras);
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log("arquivo criado");
+  } catch (erro) {
+    throw erro;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-export function contaPalavras(texto){
-    const paragrafos = extraiParagrafos(texto);
-    const contagem = paragrafos.flatMap((paragrafo) => {
-    if(!paragrafo) return [];    
+export function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto);
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
     return verificaPalavrasDuplicadas(paragrafo);
-    })
-    console.log(contagem);
+  });
+
+  return contagem;
 }
 
-function extraiParagrafos(texto){
-    return texto.toLowerCase().split('\n');
+function extraiParagrafos(texto) {
+  return (paragrafos = texto.toLowerCase().split("\n"));
 }
 
-function limpaPalavras(palavra){
-    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'');
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 }
 
-function verificaPalavrasDuplicadas(texto){
-    const listaPalavras = texto.split(' ');
-    const resultado = {};
-    // resultado[listaPalavras] = valor
-    listaPalavras.forEach(palavra => {
-        if(palavra.length >= 3){
-            const palavraLimpa = limpaPalavras(palavra);
-            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
-        }
-    })
-    return resultado;    
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  // resultado[listaPalavras] = valor
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+  return resultado;
 }
 //pegar as palavras da string e fazer virar array
 //contador de ocorrências
@@ -39,7 +40,6 @@ function verificaPalavrasDuplicadas(texto){
 //flatMap é um método de array que "achata" os resultados do lado de dentro de um array.
 // exemplo: Array = [1,2,[3,4]] após o flatMap vira
 // Array = [1,2,3,4]. No nosso exemplo a baixo ele permite que os [] vazios sejam eliminados da contagem. Ele como se ele pulasse os valores falsy.
-
 
 //replace é uma ferramenta de tratar strings que substitui por algo específico string.replace('item velho','novo item')
 //o que está no parênteses no primeiro campo é um amontoado de caracteres chamado de expressões regulares ou regeX ou regExp... o /g do final manda ele fazer isso de modo global.
