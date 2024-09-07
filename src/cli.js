9dade5a0e1ae58b9fb8cc448b7e505e2f6970a3b
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2024-09-07 15:47:22 -0300

    alterado o valor de enderecoDestino para outro padrao de endereços para testar.

--- src/cli.js
+++ src/cli.js
@@ -11,21 +11,21 @@ import { contaPalavras } from "./index.js";
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const enderecoDestino = caminhoArquivo[3];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
   try {
     if (erro) throw erro;
-    console.log("chegou aqui");
     const resultado = contaPalavras(texto);
+    console.log("Debug:chegou aqui");
     criaESalvaArquivo(resultado, endereco);
   } catch (erro) {
     trataErros(erro);
   }
 });
 
-async function criaESalvaArquivo(listaPalavras, endereco) {
-  const arquivoNovo = `${endereco}/resultado.txt`;
+async function criaESalvaArquivo(listaPalavras, enderecoDestino) {
+  const arquivoNovo = `${enderecoDestino}/resultado.txt`;
   const textoPalavras = JSON.stringify(listaPalavras);
   try {
     await fs.promises.writeFile(arquivoNovo, textoPalavras);
